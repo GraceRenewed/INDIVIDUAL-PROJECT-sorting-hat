871c8bd111b0daa7d2060172f2b89021a033f517
Christina Vieau <cvieau@live.com>
2024-07-27 15:07:00 -0400

    Main files and general structure

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log("Test")
+
+const Houses [
+
+]
+
+
+
+//  //a function to take information that is selected or created and place it on the DOM, uses the new item Id and the text on the card
+//  const renderToDom = (divId, cardToRender) => {
+//     //creates a variable and makes it equal to the element we want to put on the DOM
+//     const selectedElement=document.querySelector(divId);
+//     //takes the new card and places it in the HTML file and replaces what is there to put on the DOM
+//     selectedElement.innerHTML = cardToRender;
+    
+//  };
+
+//  const sortBtn = () => {
+//     const domString = 
+
+
+
+
+
+
+//     //Calling the function and telling it to put the information in the HTML id and placing it on the DOM
+//     renderToDom("#schoolAssignments", domString);
+ //}
